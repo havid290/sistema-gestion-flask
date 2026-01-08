835097c2ac2c43ca5414c7388bd51bfdd3ecbb23
Havid Alvarez <bhhavid@gmail.com>
2026-01-07 22:34:20 -0500

    Modelo de usuarioy base de datos con SQLA1chemy

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Configuración DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# MODELO
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    rol = db.Column(db.String(50), default='usuario')
+
+    def __repr__(self):
+        return f'<User {self.email}>'
 
 @app.route('/')
 def index():
@@ -8,3 +26,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
